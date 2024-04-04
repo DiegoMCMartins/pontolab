cfc1f412f450c7bdb1fe5c01edaed2619e20356e
Diego Miguel <diegomiguel2.8@hotmail.com>
2024-04-03 22:39:45 -0300

    feat: Criacao do escopo inicial

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import Root from './src';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('pontolab', () => Root);
---
+++ src/screens/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ScreenNavigator() {
+  return <>{null}</>;
+}
